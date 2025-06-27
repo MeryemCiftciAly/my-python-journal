b4c55319b2e8779d01859fe39da5e8aa7ea10291
MeryemCiftciAly <merygiwork3@gmail.com>
2025-06-27 21:25:22 +0300

    Add variables lesson and output

---
+++ Week1/variables.py
@@ -0,0 +1,19 @@
+
+name = "Maria"
+language = "Python"
+print("My name is", name)
+print(name, "is learning", language)
+print(name, "wants to become",language, "expert")
+
+name = "Reyhan"
+language = "English"
+print("My name is", name)
+print(name, "is learning", language)
+print(name, "wants to become",language, "expert")
+
+print()
+
+email = "info@datawithbaraa.com"
+email2 = "support@datawithbaraa.com"
+print (email + "\n"  + email2, "\n" "www.datawithbaraa.com")
+
